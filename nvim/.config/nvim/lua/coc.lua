6d013e2014f1e622e12d9df6cab6dac024837e3f
Derek Daniels <dbdaniel@mtu.edu>
2025-07-03 12:46:52 -0400

    actually fix coc

--- nvim/.config/nvim/lua/coc.lua
+++ nvim/.config/nvim/lua/coc.lua
@@ -1,3 +1,4 @@
-let g:coc_global_extensions = ['coc-json', 'coc-git', 'coc-html', '@yaegassy/coc-tailwindcss3','coc-tsserver','coc-svelte','coc-css']
+vim.g.coc_global_extensions={'coc-json', 'coc-git', 'coc-html', '@yaegassy/coc-tailwindcss3', 'coc-tsserver', 'coc-svelte','coc-css'}
+--, 'coc-html', '@yaegassy/coc-tailwindcss3', 'coc-tsserver', 'coc-svelte','coc-css']
 
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -114,3 +114,4 @@ case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
+
